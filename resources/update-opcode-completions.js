c88240c07edf9705a6c84e7241a266bf2b853326
Nathan Whetsell <nathan.whetsell@gmail.com>
2016-05-21 09:00:21 -0400

    Update completions for Csound 6.07

--- resources/update-opcode-completions.js
+++ resources/update-opcode-completions.js
@@ -1,3 +1,9 @@
+const csound = require('csound-api');
+const fs = require('fs');
+const libxml = require('libxmljs');
+const path = require('path');
+const stripBom = require('strip-bom');
+
 // Use OpcodeInfo objects to organize opcode information for creating
 // completions.
 function OpcodeInfo(opcodeEntry) {
@@ -10,18 +16,18 @@ Object.defineProperties(OpcodeInfo.prototype, {
   addInfoForOpcodeEntry: {
     value: function(opcodeEntry) {
       // Ignore array inputs.
-      var inputTypeString = opcodeEntry.intypes.replace(/\[\]/g, '');
+      const inputTypeString = opcodeEntry.intypes.replace(/\[\]/g, '');
       if (this.inputTypeStrings.indexOf(inputTypeString) < 0) {
         this.inputTypeStrings.push(inputTypeString);
       }
       // According to
       // https://github.com/csound/csound/blob/develop/Engine/entry1.c, the
       // output type 's' is deprecated but means either an a- or k-rate output.
-      var outputTypes = (opcodeEntry.outypes === 's') ? ['a', 'k'] : [opcodeEntry.outypes];
-      for (var outputType of outputTypes) {
-        var inputTypeStringsByLength = this.inputTypeStringsByLengthByOutputTypeString[outputType];
+      const outputTypes = (opcodeEntry.outypes === 's') ? ['a', 'k'] : [opcodeEntry.outypes];
+      for (const outputType of outputTypes) {
+        let inputTypeStringsByLength = this.inputTypeStringsByLengthByOutputTypeString[outputType];
         if (inputTypeStringsByLength) {
-          var inputTypeStrings = inputTypeStringsByLength[inputTypeString.length];
+          const inputTypeStrings = inputTypeStringsByLength[inputTypeString.length];
           if (inputTypeStrings) {
             if (inputTypeStrings.indexOf(inputTypeString) < 0) {
               inputTypeStrings.push(inputTypeString);
@@ -40,7 +46,7 @@ Object.defineProperties(OpcodeInfo.prototype, {
 });
 
 // Create a map from opcode names to opcode properties.
-var ignoredOpcodes = [
+const ignoredOpcodes = [
   // Function opcodes
   'a',
   'abs',
@@ -114,6 +120,7 @@ var ignoredOpcodes = [
   'rigoto',
   'tigoto',
   // Opcodes that use comma-separated lists of arguments of any type
+  'changed2',
   'framebuffer',
   'hdf5read',
   'hdf5write',
@@ -122,13 +129,12 @@ var ignoredOpcodes = [
   // Opcodes with input arguments that are difficult to parse
   'alwayson'
 ];
-var opcodeInfoByName = {};
-var csound = require('csound-api');
-var opcodeList = [];
+const opcodeInfoByName = {};
+const opcodeList = [];
 csound.NewOpcodeList(csound.Create(), opcodeList);
-for (var opcodeEntry of opcodeList) {
+for (const opcodeEntry of opcodeList) {
   if (ignoredOpcodes.indexOf(opcodeEntry.opname) < 0) {
-    var opcodeInfo = opcodeInfoByName[opcodeEntry.opname];
+    const opcodeInfo = opcodeInfoByName[opcodeEntry.opname];
     if (opcodeInfo) {
       opcodeInfo.addInfoForOpcodeEntry(opcodeEntry);
     } else {
@@ -195,10 +201,10 @@ function InputArgumentInfo(inputTypeStrings, nameArray) {
   // .  required argument of any type, used by init (for arrays), lenarray,
   //    print_type, and slicearray
   // ?  optional argument of any type, possibly unused
-  // *  comma-separated list of arguments of any type, used by framebuffer,
-  //    hdf5read, hdf5write, xin, and xout
+  // *  comma-separated list of arguments of any type, used by changed2,
+  //    framebuffer, hdf5read, hdf5write, xin, and xout
 
-  var typeInfoByType = {
+  const typeInfoByType = {
     'a': {strings: ['a'], optional: false, list: false},
     'B': {strings: ['B'], optional: false, list: false},
     'f': {strings: ['f'], optional: false, list: false},
@@ -230,15 +236,15 @@ function InputArgumentInfo(inputTypeStrings, nameArray) {
     'z': {strings: ['k'], optional: false, list: true},
     '.': {strings: ['a', 'i', 'k', 'S'], optional: false, list: false}
   };
-  for (var inputTypeString of inputTypeStrings) {
+  for (const inputTypeString of inputTypeStrings) {
     if (inputTypeString.length > 0) {
-      var inputTypes = inputTypeString.match(/(?:[aBfhijklOoPpqSTUVvwx\.]|[MmNnWyZz]+)/g);
-      for (var i = 0, length = inputTypes.length; i < length; i++) {
-        var typeInfo = typeInfoByType[inputTypes[i]];
+      const inputTypes = inputTypeString.match(/(?:[aBfhijklOoPpqSTUVvwx\.]|[MmNnWyZz]+)/g);
+      for (let i = 0, length = inputTypes.length; i < length; i++) {
+        const typeInfo = typeInfoByType[inputTypes[i]];
 
-        var typeStrings = this.typeStringsArray[i];
+        const typeStrings = this.typeStringsArray[i];
         if (typeStrings) {
-          for (var string of typeInfo.strings) {
+          for (const string of typeInfo.strings) {
             if (typeStrings.indexOf(string) < 0) {
               typeStrings.push(string);
             }
@@ -254,20 +260,20 @@ function InputArgumentInfo(inputTypeStrings, nameArray) {
   }
 }
 
-var openingBracket = '(';
-var separator = '|';
-var closingBracket = ')';
+const openingBracket = '(';
+const separator = '|';
+const closingBracket = ')';
 Object.defineProperties(InputArgumentInfo.prototype, {
   displayText: {
     get: function() {
-      var length = this.typeStringsArray.length;
+      const length = this.typeStringsArray.length;
       if (length === 0) {
         return '';
       }
 
-      var components = [];
-      for (var i = 0; i < length; i++) {
-        var string = this.typeStringsArray[i].join(separator);
+      const components = [];
+      for (let i = 0; i < length; i++) {
+        let string = this.typeStringsArray[i].join(separator);
         if (string.length > 1) {
           string = openingBracket + string + closingBracket;
         }
@@ -282,16 +288,15 @@ Object.defineProperties(InputArgumentInfo.prototype, {
 
   snippet: {
     get: function() {
-      var length = this.typeStringsArray.length;
+      const length = this.typeStringsArray.length;
       if (length === 0) {
         return '';
       }
 
-      var snippet = '';
-      var optionalIndex = null;
-      for (var i = 0; i < length; i++) {
-        var types = this.typeStringsArray[i].join(separator);
-        var string = types;
+      let snippet = '';
+      let optionalIndex = null;
+      for (let i = 0; i < length; i++) {
+        let string = this.typeStringsArray[i].join(separator);
         if (string.length > 1) {
           string = openingBracket + string + closingBracket;
         }
@@ -333,63 +338,59 @@ function formatOutputTypeString(string) {
 }
 
 // Create a list of Csound Manual XML file names.
-var fs = require('fs');
-var path = require('path');
-var opcodesPath = path.join('csound', 'manual', 'opcodes');
-var opcodeXMLFileNames = fs.readdirSync(opcodesPath).map(function(opcodePath) {
-  return path.parse(opcodePath).name;
-});
+const opcodesPath = path.join('csound', 'manual', 'opcodes');
+const opcodeXMLFileNames = fs.readdirSync(opcodesPath).map(opcodePath => path.parse(opcodePath).name);
 
 // Csound manual XML files describing opcodes need a header to be parsed without
 // errors.
-var XMLHeader = [
-  '<?xml version="1.0" encoding="utf-8"?>',
-  '<!DOCTYPE refentry [',
+const XMLHeader = `<?xml version="1.0" encoding="utf-8"?>
+  <!DOCTYPE refentry [
 
-  // From http://dev.w3.org/html5/html-author/charref
-  '<!ENTITY auml "ä">',
-  '<!ENTITY beta "β">',
-  '<!ENTITY circ "ˆ">',
-  '<!ENTITY lambda "λ">',
-  '<!ENTITY le "≤">',
-  '<!ENTITY nbsp "&#xA0;">',
-  '<!ENTITY num "#">',
-  '<!ENTITY percnt "&#x25;">',
-  '<!ENTITY pi "π">',
-  '<!ENTITY plusmn "±">',
-  '<!ENTITY shy "&#xAD;">',
-  '<!ENTITY tilde "˜">',
+  <!-- From http://dev.w3.org/html5/html-author/charref -->
+  <!ENTITY auml "ä">
+  <!ENTITY beta "β">
+  <!ENTITY circ "^">
+  <!ENTITY lambda "λ">
+  <!ENTITY le "≤">
+  <!ENTITY nbsp "&#xA0;">
+  <!ENTITY num "#">
+  <!ENTITY percnt "&#x25;">
+  <!ENTITY pi "π">
+  <!ENTITY plusmn "±">
+  <!ENTITY shy "&#xAD;">
+  <!ENTITY tilde "˜">
 
-  // From https://github.com/csound/manual/blob/master/manual.xml
-  '<!ENTITY nameandres "Andrés Cabrera">',
-  '<!ENTITY nameanthony "Anthony Kozar">',
-  '<!ENTITY namebarry "Barry L. Vercoe">',
-  '<!ENTITY namegabriel "Gabriel Maldonado">',
-  '<!ENTITY namehans "Hans Mikelson">',
-  '<!ENTITY nameistvan "Istvan Varga">',
-  '<!ENTITY namejohn "John ffitch">',
-  '<!ENTITY namekanata "Kanata Motohashi">',
-  '<!ENTITY namekevin "Kevin Conder">',
-  '<!ENTITY nameluis "Luis Jure">',
-  '<!ENTITY namematt "Matt Ingalls">',
-  '<!ENTITY namemichael "Michael Gogins">',
-  '<!ENTITY namemike "Mike Berry">',
-  '<!ENTITY nameoeyvind "Øyvind Brandtsegg">',
-  '<!ENTITY nameparis "Paris Smaragdis">',
-  '<!ENTITY nameperry "Perry Cook">',
-  '<!ENTITY namepeter "Peter Brinkmann">',
-  '<!ENTITY namepinot "François Pinot">',
-  '<!ENTITY namerasmus "Rasmus Ekman">',
-  '<!ENTITY namerichard "Richard Dobson">',
-  '<!ENTITY namesean "Sean Costello">',
-  '<!ENTITY namesteven "Steven Yi">',
-  '<!ENTITY nametito "Tito Latini">',
-  '<!ENTITY namevictor "Victor Lazzarini">',
-  ']>'
-].join('\n');
+  <!-- From https://github.com/csound/manual/blob/master/manual.xml -->
+  <!ENTITY nameandres "Andrés Cabrera">
+  <!ENTITY nameanthony "Anthony Kozar">
+  <!ENTITY namebarry "Barry L. Vercoe">
+  <!ENTITY namegabriel "Gabriel Maldonado">
+  <!ENTITY namehans "Hans Mikelson">
+  <!ENTITY nameistvan "Istvan Varga">
+  <!ENTITY namejohn "John ffitch">
+  <!ENTITY namekanata "Kanata Motohashi">
+  <!ENTITY namekevin "Kevin Conder">
+  <!ENTITY nameluis "Luis Jure">
+  <!ENTITY namematt "Matt Ingalls">
+  <!ENTITY namemichael "Michael Gogins">
+  <!ENTITY namemike "Mike Berry">
+  <!ENTITY nameoeyvind "Øyvind Brandtsegg">
+  <!ENTITY nameparis "Paris Smaragdis">
+  <!ENTITY namepaul "Paul Batchelor">
+  <!ENTITY nameperry "Perry Cook">
+  <!ENTITY namepeter "Peter Brinkmann">
+  <!ENTITY namepinot "François Pinot">
+  <!ENTITY namerasmus "Rasmus Ekman">
+  <!ENTITY namerichard "Richard Dobson">
+  <!ENTITY namesean "Sean Costello">
+  <!ENTITY namesteven "Steven Yi">
+  <!ENTITY nametito "Tito Latini">
+  <!ENTITY namevictor "Victor Lazzarini">
+  ]>
+`;
 
 // Create a map of more descriptive input argument names.
-var descriptiveNamesByName = {
+const descriptiveNamesByName = {
   'amp': 'Amplitude',
   'atdec': 'AttenuationFactor',
   'cps': 'Frequency',
@@ -407,74 +408,72 @@ var descriptiveNamesByName = {
   'sig': 'Signal'
 };
 
-var libxml = require('libxmljs');
-var stripBom = require('strip-bom');
-var allCompletions = [];
-for (var opcodeName in opcodeInfoByName) {
+let allCompletions = [];
+for (const opcodeName in opcodeInfoByName) {
+  let inputArgumentNames = [];
+
   if (opcodeInfoByName.hasOwnProperty(opcodeName) && opcodeXMLFileNames.indexOf(opcodeName) >= 0) {
     // Get opcode descriptions and input argument names from the Csound Manual
     // XML file.
-    var XMLString = fs.readFileSync(path.join(opcodesPath, opcodeName + '.xml'), 'utf8');
+    let XMLString = fs.readFileSync(path.join(opcodesPath, `${opcodeName}.xml`), 'utf8');
     // Csound manual XML files may include a Unicode byte order mark (BOM).
     XMLString = stripBom(XMLString);
-    var XMLDocument = libxml.parseXmlString(XMLHeader + XMLString);
-    var description = XMLDocument.get('//refpurpose').text().trim().replace(/\s+/g, ' ');
+    const XMLDocument = libxml.parseXmlString(XMLHeader + XMLString);
+    const description = XMLDocument.get('//refpurpose').text().trim().replace(/\s+/g, ' ');
     // Assume the XML document contains at least one synopsis element with one
     // child command element.
-    var commandElement = XMLDocument.get('//synopsis/command');
+    const commandElement = XMLDocument.get('//synopsis/command');
     if (commandElement) {
       // Assume the next sibling of the command element is a text node
       // describing input arguments.
-      var node = commandElement.nextSibling();
+      const node = commandElement.nextSibling();
       if (node && node.type() === 'text') {
-        var inputArgumentNames = node.toString().replace(/[\\\[\]]/g, '').replace(/\s+/g, ' ').split(',');
-        for (var i = 0, length = inputArgumentNames.length; i < length; i++) {
-          var name = inputArgumentNames[i].trim();
+        inputArgumentNames = node.toString().replace(/[\\\[\]]/g, '').replace(/\s+/g, ' ').split(',');
+        for (let i = 0, length = inputArgumentNames.length; i < length; i++) {
+          let name = inputArgumentNames[i].trim();
           if (name.charAt(0) === '"' && name.charAt(name.length - 1) === '"') {
             // If the name is enclosed in double quotes, remove the quotes and
             // create a camel-case name.
-            inputArgumentNames[i] = name.replace(/"/g, '').split(' ').map(function(string) {
-              return string.charAt(0).toUpperCase() + string.slice(1);
-            }).join('');
+            inputArgumentNames[i] = name.replace(/"/g, '').split(' ').map(string => string.charAt(0).toUpperCase() + string.slice(1)).join('');
           } else {
             // Otherwise, remove the first two characters if the first character
             // is a g (indicating a global variable), and just the first
             // character otherwise.
             name = name.slice((name.charAt(0) === 'g') ? 2 : 1);
-            var descriptiveName = descriptiveNamesByName[name];
+            const descriptiveName = descriptiveNamesByName[name];
             inputArgumentNames[i] = descriptiveName ? descriptiveName : name;
           }
         }
       }
     }
 
-    var completions = [];
-    var opcodeInfo = opcodeInfoByName[opcodeName];
-    var inputTypeStringsByLengthByOutputTypeString = opcodeInfo.inputTypeStringsByLengthByOutputTypeString;
+    const completions = [];
+    const opcodeInfo = opcodeInfoByName[opcodeName];
+    const inputTypeStringsByLengthByOutputTypeString = opcodeInfo.inputTypeStringsByLengthByOutputTypeString;
 
     // If all input type strings are equal, then the inputTypeStrings property
     // of opcodeInfo will be a length-1 array.
     if (opcodeInfo.inputTypeStrings.length === 1) {
       // Create a single completion for all output type strings.
-      var completion = {};
-      var leftLabel = formatOutputTypeString(Object.getOwnPropertyNames(inputTypeStringsByLengthByOutputTypeString).join(''));
+      const completion = {};
+      const leftLabel = formatOutputTypeString(Object.getOwnPropertyNames(inputTypeStringsByLengthByOutputTypeString).join(''));
       if (leftLabel.length > 0) {
         completion.leftLabel = leftLabel;
       }
-      var inputArgumentInfo = new InputArgumentInfo(opcodeInfo.inputTypeStrings, inputArgumentNames);
+      const inputArgumentInfo = new InputArgumentInfo(opcodeInfo.inputTypeStrings, inputArgumentNames);
       completion.snippet = opcodeName + inputArgumentInfo.snippet;
       completion.displayText = opcodeName + inputArgumentInfo.displayText;
       completions.push(completion);
     } else {
       // Create completions for each output type string and input type string
       // length.
-      for (var outputTypeString in inputTypeStringsByLengthByOutputTypeString) {
+      for (const outputTypeString in inputTypeStringsByLengthByOutputTypeString) {
         if (inputTypeStringsByLengthByOutputTypeString.hasOwnProperty(outputTypeString)) {
-          var inputTypeStringsByLength = inputTypeStringsByLengthByOutputTypeString[outputTypeString];
-          for (var length in inputTypeStringsByLength) {
-            var completion = {leftLabel: formatOutputTypeString(outputTypeString)};
+          const inputTypeStringsByLength = inputTypeStringsByLengthByOutputTypeString[outputTypeString];
+          for (const length in inputTypeStringsByLength) {
+            const completion = {leftLabel: formatOutputTypeString(outputTypeString)};
             if (inputTypeStringsByLength.hasOwnProperty(length)) {
-              var inputArgumentInfo = new InputArgumentInfo(inputTypeStringsByLength[length], inputArgumentNames);
+              const inputArgumentInfo = new InputArgumentInfo(inputTypeStringsByLength[length], inputArgumentNames);
               completion.snippet = opcodeName + inputArgumentInfo.snippet;
               completion.displayText = opcodeName + inputArgumentInfo.displayText;
               completions.push(completion);
@@ -485,16 +484,16 @@ for (var opcodeName in opcodeInfoByName) {
     }
 
     // Add additional completion properties from the Csound Manual XML file.
-    for (var completion of completions) {
+    for (const completion of completions) {
       completion.opcode = opcodeName;
       completion.description = description;
-      completion.descriptionMoreURL = 'https://csound.github.io/docs/manual/' + opcodeName + '.html';
+      completion.descriptionMoreURL = `https://csound.github.io/docs/manual/${opcodeName}.html`;
       completion.type = 'function';
     }
 
     allCompletions = allCompletions.concat(completions);
   } else {
-    console.log('Skipping opcode "' + opcodeName + '" returned by csoundNewOpcodeList() because "' + opcodeName + '.xml" is not in the Csound manual repository.');
+    console.log(`Skipping opcode ${opcodeName} because ${opcodeName}.xml is not in the Csound manual repository.`);
   }
 }
 
@@ -506,7 +505,16 @@ allCompletions = allCompletions.concat([
     displayText: 'alwayson (i|S)Instrument, p4, p5, …',
     opcode: 'alwayson',
     description: 'Activates the indicated instrument in the orchestra header.',
-    descriptionMoreURL: 'http://csound.github.io/docs/manual/alwayson.html',
+    descriptionMoreURL: 'https://csound.github.io/docs/manual/alwayson.html',
+    type: 'function'
+  },
+  {
+    leftLabel: 'k[]',
+    snippet: 'changed2 ${1:kVariableName1/*, kVariableName2, …*/}',
+    displayText: 'changed2 kVariableName1, kVariableName2, …',
+    opcode: 'changed2',
+    description: 'k-rate signal change detector.',
+    descriptionMoreURL: 'https://csound.github.io/docs/manual/changed2.html',
     type: 'function'
   },
   {
@@ -515,6 +523,7 @@ allCompletions = allCompletions.concat([
     displayText: 'framebuffer aInput, iSize',
     opcode: 'framebuffer',
     description: 'Read audio signals into 1 dimensional k-rate arrays and vice-versa with a specified buffer size.',
+    descriptionMoreURL: 'https://csound.github.io/docs/manual/framebuffer.html',
     type: 'function'
   },
   {
@@ -523,6 +532,7 @@ allCompletions = allCompletions.concat([
     displayText: 'framebuffer kInput, iSize',
     opcode: 'framebuffer',
     description: 'Read audio signals into 1 dimensional k-rate arrays and vice-versa with a specified buffer size.',
+    descriptionMoreURL: 'https://csound.github.io/docs/manual/framebuffer.html',
     type: 'function'
   },
   {
@@ -562,7 +572,7 @@ allCompletions = allCompletions.concat([
 ]);
 
 // Write the completions dictionary to a JSON file.
-var fileDescriptor = fs.openSync('opcode-completions.json', 'w');
+const fileDescriptor = fs.openSync('opcode-completions.json', 'w');
 fs.writeSync(fileDescriptor, JSON.stringify({
   about: 'The contents of this file are derived from the source files of The Canonical Csound Reference Manual <https://github.com/csound/manual>. The Canonical Csound Reference Manual is licensed under the terms of the GNU Free Documentation License, Version 1.2 or any later version published by the Free Software Foundation; with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts. Copyrights of The Canonical Csound Reference Manual are held by the Massachusetts Institute of Technology (1986, 1992), Kevin Conder (2003), and others noted in individual source files. This file is licensed under the terms of the GNU Free Documentation License, Version 1.2 or any later version published by the Free Software Foundation; with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.',
   completions: allCompletions
