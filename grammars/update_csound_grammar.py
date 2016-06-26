e3629403e9d7a388083029dd629ae67c1bca66ad
Nathan Whetsell <nathan.whetsell@gmail.com>
2016-06-26 12:34:43 -0400

    Highlight opcode type annotations

--- grammars/update_csound_grammar.py
+++ grammars/update_csound_grammar.py
@@ -6,13 +6,11 @@ from regexopt import regex_opt
 
 pattern = r"""
       { # This pattern should be updated using update_csound_grammar\.py\.
-        name: 'support\.function\.csound'
         match: [^\n]+
       }"""
 replacement = """
       { # This pattern should be updated using update_csound_grammar.py.
-        name: 'support.function.csound'
-        match: '""" + regex_opt(OPCODES, r'\\\\b', r'\\\\b').replace('\\_', '_') + """'
+        match: '""" + regex_opt(OPCODES, r'\\\\b', r'\\\\b').replace('\\_', '_') + """(?:(\\:)([A-Za-z]))?'
       }"""
 with open('csound.cson', 'r') as file:
     grammar = file.read()
